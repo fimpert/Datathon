3bf4ddcccf6f6550bce60d2323870bd75b2beec4
Dorah Wang <duorah@Dorahs-MacBook-Pro.local>
2026-02-07 14:35:21 -0800

    nicer looking graphs

--- k_cluster.py
+++ k_cluster.py
@@ -3,6 +3,8 @@ import geopandas as gpd
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
+from shapely.affinity import scale, translate
+
 
 # load excel file
 df = pd.read_excel('Access_to_a_Livable_Planet_Dataset.xlsx')
@@ -14,14 +16,14 @@ df["County"] = (df["County"].str.lower().str.replace(" County", "", regex=False)
 df["State"] = (df["State"].str.lower().str.replace(" State", "", regex=False).str.strip())
 
 # renaming crosswalk columns and normalizing county and state names
-crosswalk = crosswalk.rename(columns={"countyname_fips": "County","state_name": "State"})
+crosswalk = crosswalk.rename(columns={"countyname_fips": "County","state_name": "State", "fipscounty": "fips"})
 crosswalk["County"] = (crosswalk["County"].str.lower().str.replace(" County", "", regex=False).str.strip())
 crosswalk["State"] = ( crosswalk["State"] .str.lower().str.replace(" State", "", regex=False).str.strip())
 
 #print(df.columns)
 #print(crosswalk.columns)
 
-# merging datasets
+# merging dataset for fips code 
 df = df.merge(
     crosswalk,
     left_on=["State", "County"],
@@ -31,7 +33,6 @@ df = df.merge(
 
 
 df = df.fillna(0)
-df = df.rename(columns={"fipscounty": "fips"})
 df['fips'] = df['fips'].astype(str).str.split('.').str[0].str.zfill(5)
 
 
@@ -47,21 +48,58 @@ k = 4
 kmeans_model = KMeans(n_clusters=k, init='k-means++', random_state=42)
 df["cluster"] = kmeans_model.fit_predict(X_scaled)
 
-
+# open US county shapefile
 counties = gpd.read_file("cb_2018_us_county_500k.shp")
-df["fips"] = df["fips"].astype(str).str.zfill(5)
 counties["GEOID"] = counties["GEOID"].astype(str).str.zfill(5)
 
+# remove carrebiean islands
+counties = counties[~counties['STATEFP'].isin(['60','66','69','72','78'])]
 
-print(df[["State", "County", "fips", "cluster"]].head())
-print(df[["State", "County", "fips", "cluster"]].tail())
-print(df["cluster"].unique())
 
-# Suppose counties shapefile has GEOID column
-counties = counties.merge(df[['fips', 'cluster']], left_on='GEOID', right_on='fips', how='left')
+# print(df[["State", "County", "fips", "cluster"]].head())
+# print(df[["State", "County", "fips", "cluster"]].tail())
+# print(df["cluster"].unique())
 
-fig, ax = plt.subplots(1, 1, figsize=(12, 8))
-counties.plot(column='cluster', cmap='tab10', legend=True, linewidth=0.5, edgecolor='black', ax=ax)
+
+# Sort clusters by mean % unhealthy days and reorder from lowest to highest
+cluster_summary = df.groupby('cluster')['Percentage of Unhealthy Days'].mean()
+ordered_clusters = cluster_summary.sort_values().index.tolist()
+cluster_order_map = {old: new for new, old in enumerate(ordered_clusters)}
+df['cluster_ordered'] = df['cluster'].map(cluster_order_map)
+
+# merge and plot
+counties = counties.merge(df[['fips', 'cluster_ordered']], left_on='GEOID', right_on='fips', how='left')
+
+# FORMATTING
+counties = counties.to_crs("EPSG:5070")
+alaska = counties[counties['STATEFP'] == '02'].copy()
+hawaii = counties[counties['STATEFP'] == '15'].copy()
+conus = counties[(counties['STATEFP'] != '02') & (counties['STATEFP'] != '15')].copy()
+alaska['geometry'] = alaska['geometry'].apply(lambda x: scale(x, xfact=0.5, yfact=0.5, origin=(0, 0)))
+alaska['geometry'] = alaska['geometry'].apply(lambda x: translate(x, xoff=-2_000_000, yoff=0))
+hawaii['geometry'] = hawaii['geometry'].apply(lambda x: translate(x, xoff=3_000_000, yoff = -1_000_000))
+counties = pd.concat([conus, alaska, hawaii])
+counties['geometry'] = counties['geometry'].apply(lambda x: scale(x, xfact=1.5, yfact=1.5, origin=(0, 0)))
+
+fig, ax = plt.subplots(1, 1, figsize=(15, 10), dpi=120)
+counties.plot(
+    column='cluster_ordered', 
+    cmap='Reds', 
+    categorical=True,
+    legend=True, 
+    linewidth=0.5, 
+    # edgecolor='black', 
+    ax=ax, 
+    legend_kwds={"title": "Cluster", "loc": "lower left", "fontsize": 10}
+)
+counties.boundary.plot(
+    ax=ax,
+    color='black',
+    linewidth=0.5
+)
+# ax.set_xlim(-180, -60)
+# ax.set_ylim(15, 75)
+
 ax.set_title("K-means Clusters of Counties")
 ax.axis('off')
 plt.show()
---
+++ random_forest_model.py
@@ -0,0 +1,74 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error, r2_score, accuracy_score, classification_report
+import matplotlib.pyplot as plt
+
+
+
+# load excel file
+df = pd.read_excel('Access_to_a_Livable_Planet_Dataset.xlsx')
+crosswalk = pd.read_csv("ssa_fips_state_county_2025.csv")
+
+# data cleaning
+# cleaning normalizing dataset county and state names
+df["County"] = (df["County"].str.lower().str.replace(" County", "", regex=False).str.strip())
+df["State"] = (df["State"].str.lower().str.replace(" State", "", regex=False).str.strip())
+
+# renaming crosswalk columns and normalizing county and state names
+crosswalk = crosswalk.rename(columns={"countyname_fips": "County","state_name": "State"})
+crosswalk["County"] = (crosswalk["County"].str.lower().str.replace(" County", "", regex=False).str.strip())
+crosswalk["State"] = ( crosswalk["State"] .str.lower().str.replace(" State", "", regex=False).str.strip())
+
+#print(df.columns)
+#print(crosswalk.columns)
+
+# merging datasets
+df = df.merge(
+    crosswalk,
+    left_on=["State", "County"],
+    right_on=["State", "County"],
+    how="left"
+)
+
+
+df = df.fillna(0)
+df = df.rename(columns={"fipscounty": "fips"})
+df['fips'] = df['fips'].astype(str).str.split('.').str[0].str.zfill(5)
+print(df.columns.tolist())
+df.columns = df.columns.str.strip() 
+
+features = [
+    'Median AQI',
+    '90th Percentile AQI',
+    'Days CO',
+    'Days NO2',
+    'Days Ozone',
+    'Days PM2.5',
+    'Days PM10',
+    'Max AQI'
+]
+x = df[features]
+y = df['Percentage of Unhealthy Days']
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+
+rf_model = RandomForestRegressor(n_estimators=100, random_state=42)
+rf_model.fit(X_train, y_train)
+
+y_pred = rf_model.predict(X_test)
+print("R2 Score:", r2_score(y_test, y_pred))
+print("RMSE:", mean_squared_error(y_test, y_pred, squared=False))
+
+
+importances = rf_model.feature_importances_
+plt.bar(features, importances)
+plt.title("Feature Importance")
+plt.show()
+
+
+y_pred = rf_model.predict(X_test)
+print("R2:", r2_score(y_test, y_pred))
+print("RMSE:", mean_squared_error(y_test, y_pred, squared=False))
