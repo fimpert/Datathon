b959e4a28e568be84a207bb2072b645a9f710c27
25Rosario <rosario.gabrielle07@gmail.com>
2026-02-07 17:15:49 -0800

    add clean data set and percentage modeling

--- Pollutant Modeling/modeling.R
+++ Pollutant Modeling/modeling.R
@@ -9,22 +9,39 @@ data <- read.csv("Access_to_a_Livable_Planet_Dataset_Clean.csv")
 getwd()
 setwd("C:/Users/rosar/Datathon/Data")
 
-# avgByYear <- function(data, year, pollutant_col) { 
-#     data %>% 
-#         #drop_na() %>%
-#         mutate(
-#             {{ pollutant_col }} := as.numeric({{ pollutant_col }})
-#         ) %>%
-#         group_by({{ year }}) %>% 
-#         summarize(avg = mean({{ pollutant_col }}, na.rm = TRUE)) }
-
-# avgByYear(data, Year, `Days.CO`)
-
 names(data)
 unique(data$State)
 
-# calculate probability of each pollutant's risk using aqi
-# number of states with an aqi over 100 / total number of states
+#calculate percentage of days that each pollutant was a main factor divided by 365 days for each county
+percentageDays <- function(data, county_col, pollutant_col) {
+  data %>%
+    group_by({{ county_col }}) %>%
+    summarize(
+      percentage = sum(as.numeric({{ pollutant_col }}), na.rm = TRUE) / 365 * 100,
+      .groups = "drop"
+    )
+}
+
+print(percentageDays(data, `County`, `Days.PM10`))
+
+# calculate which pollutant poses the high longest risk by adding the percentages of each county and taking the average
+pollutants <- c("Days.CO", "Days.NO2", "Days.Ozone", "Days.PM2.5", "Days.PM10")
+average_percentage <- data.frame()
+
+for (pollutant in pollutants) {
+  percentage <- percentageDays(data, County, !!sym(pollutant))
+  average_percentage <- rbind(
+    average_percentage,
+    data.frame(
+      Pollutant = pollutant,
+      Average_Percentage = mean(percentage$percentage, na.rm = TRUE)
+    )
+  )
+}
+
+# Print once
+print(average_percentage)
+
 
 
 
