235dfbab7c2871d7e72ef99e9e8e6edc94e8b279
Dorah Wang <duorah@Dorahs-MacBook-Pro.local>
2026-02-07 20:46:26 -0800

    delete unwanted files

--- k cluster.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from sklearn.cluster import KMeans
-from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt
-
-# load excel file
-df = pd.read_excel('')
